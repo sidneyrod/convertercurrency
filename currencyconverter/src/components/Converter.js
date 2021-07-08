e60efc2cc90139522bb1b5f9e840a1cd4035054b
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-07-08 12:12:18 -0300

    Creating Converter component

---
+++ currencyconverter/src/components/Converter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Converter extends Component {
+    render() {
+        return(
+            <div className="Converter">
+                <p>This is our converter</p>
+            </div>
+        )
+    }
+}
