3e61e3c8a24c976f9122987ffc841175c2dd31e5
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-07-08 19:27:15 -0300

    Creating props, event, button

--- currencyconverter/src/components/Converter.js
+++ currencyconverter/src/components/Converter.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 export default class Converter extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            coinA_value: "",
+            coinB_value: 0
+        }
+    }
+
+    converter() {
+        
+    }
+
     render() {
         return(
             <div className="Converter">
-                <p>This is our converter</p>
+                <h2>{this.props.coinA} to {this.props.coinB}</h2>
+                <input type="text" onChange={(event)=>{this.setState({coinA_value:event.target.value})}}></input>
+                <input type="button" value="Convert" onClick={this.converter}></input>
+                <h2>converted value</h2>
             </div>
         )
     }
