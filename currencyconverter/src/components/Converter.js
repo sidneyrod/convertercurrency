c52deee9b2e1f23f8971724f88fd2e11dd37a25f
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-07-09 09:45:24 -0300

    Insert url and calculation rule to convert

--- currencyconverter/src/components/Converter.js
+++ currencyconverter/src/components/Converter.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './Converter.css';
 
 export default class Converter extends Component {
 
@@ -7,21 +9,35 @@ export default class Converter extends Component {
 
         this.state = {
             coinA_value: "",
-            coinB_value: 0
+            coinB_value: 0,
         }
+
+        this.converter = this.converter.bind(this);
     }
 
     converter() {
         
+        let from_to = `${this.props.coinA}_${this.props.coinB}`;
+        let url = `https://free.currconv.com/api/v7/convert?q=${from_to}&compact=ultra&apiKey=e8fce720b0b87a7bfaaa`;
+
+        fetch(url)
+        .then(res=>{
+            return res.json()
+        })
+        .then(json=>{
+            let price = json[from_to];
+            let coinB_value = (parseFloat(this.state.coinA_value) * price).toFixed(2);
+            this.setState({coinB_value});
+        })
     }
 
     render() {
         return(
-            <div className="Converter">
-                <h2>{this.props.coinA} to {this.props.coinB}</h2>
+            <div className="converter">
+                <h2>{this.props.coinA} / {this.props.coinB}</h2>
                 <input type="text" onChange={(event)=>{this.setState({coinA_value:event.target.value})}}></input>
-                <input type="button" value="Convert" onClick={this.converter}></input>
-                <h2>converted value</h2>
+                <input  type="button" value="Convert" onClick={this.converter}></input>
+                <h2>{this.state.coinB_value}</h2>
             </div>
         )
     }
